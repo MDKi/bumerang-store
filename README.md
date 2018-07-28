b9c38244b8c899b7cac23cecb1d12e01d5c204c0
Gabriel Fuertes <gabifuertes@gmail.com>
2018-07-28 13:20:42 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+Just a simple inventory management API.
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(bodyParser.json());
+
+// Database
+const dbURL = 'mongodb://localhost/bumerang-store';
+mongoose.connect(dbURL);
+const db = mongoose.connection;
+
+// For testing purposes
+app.get('/api', (req, res) => {
+	res.send('Working! 😊');
+});
+
+app.listen(3000);
