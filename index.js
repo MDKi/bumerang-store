735c6b5119f352af8ea4200584abcd4b540c7472
Gabriel Fuertes <gabifuertes@gmail.com>
2018-08-06 15:57:29 -0300

    Added GET and POST requests.

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+// let route = require('routes');
+// app.use("/", route);
+
 app.use(bodyParser.json());
+
+const Product = require('./models/product');
 
 // Database
 const dbURL = 'mongodb://localhost/bumerang-store';
@@ -14,5 +19,35 @@ const db = mongoose.connection;
 app.get('/api', (req, res) => {
   res.send('Working! 😊');
 });
+
+app.get('/api/products', (req, res) => {
+  Product.getProducts((err, products) => {
+    if(err) {
+      throw err;
+    }
+    res.json(products);
+  })
+});
+
+app.get('/api/products/:_id', (req, res) => {
+  Product.getProductById(req.params._id, (err, product) => {
+    if(err) {
+      throw err;
+    }
+    res.json(product);
+  })
+})
+
+app.post('/api/products', (req, res) => {
+  let product = req.body;
+  Product.addProduct(product, (err, product) => {
+    if(err) {
+      throw err;
+    }
+    res.json(product);
+  })
+});
+// app.put();
+// app.delete();
 
 app.listen(3000);
---
+++ models/product.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String
+  },
+  create_date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Product = module.exports = mongoose.model('Product', productSchema);
+
+
+module.exports.getProducts = (callback, limit) => {
+	Product.find(callback).limit(limit);
+}
+
+module.exports.getProductById = (id, callback) => {
+	Product.findById(id, callback);
+}
+
+module.exports.addProduct = (product, callback) => {
+	Product.create(product, callback);
+}
+
+module.exports.updateProduct = (id, product, options, callback) => {
+	const query = {_id: id};
+	const update = {
+    name: product.name
+	}
+	Product.findOneAndUpdate(query, update, options, callback);
+}
+
+module.exports.removeProduct = (id, callback) => {
+	var query = {_id: id};
+	Product.remove(query, callback);
+}
