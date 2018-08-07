8cb4bfaaafa2f1164bc0437d01d187e30ca80dde
Alvaro Fresquet <alvarofresquet@gmail.com>
2018-08-07 18:33:04 -0300

    moved products' routes to their own file

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(bodyParser.json());
 
 const Product = require('./models/product');
 
+const products = require("./routes/products");
+
 // Database
 const dbURL = 'mongodb://localhost:27017/bumerang-store';
 mongoose.connect(dbURL, { useNewUrlParser: true });
@@ -17,53 +19,6 @@ app.get('/api', (req, res) => {
   res.send('Working! 😊');
 });
 
-app.get('/api/products', (req, res) => {
-  Product.getProducts((err, products) => {
-    if(err) {
-      throw err;
-    }
-    res.json(products);
-  });
-});
-
-app.get('/api/products/:_id', (req, res) => {
-  Product.getProductById(req.params._id, (err, product) => {
-    if(err) {
-      throw err;
-    }
-    res.json(product);
-  });
-});
-
-app.post('/api/products', (req, res) => {
-  let product = req.body;
-  Product.addProduct(product, (err, product) => {
-    if(err) {
-      throw err;
-    }
-    res.json(product);
-  });
-});
-
-app.put('/api/products/:_id', (req, res) => {
-  const id = req.params._id;
-  const product = req.body;
-  Product.updateProduct(id, product, {}, (err, product) => {
-    if(err) {
-      throw err;
-    }
-    res.json(product);
-  });
-});
-
-app.delete('/api/products/:_id', (req, res) => {
-  const id = req.params._id;
-  Product.removeProduct(id, (err, product) => {
-    if(err) {
-      throw err;
-    }
-    res.json(product);
-  });
-});
+app.use("/api/products", products);
 
 app.listen(3000);
---
+++ routes/products.js
@@ -0,0 +1,54 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Product.getProducts((err, products) => {
+    if (err) {
+      throw err;
+    }
+    res.json(products);
+  });
+});
+
+router.get("/:_id", (req, res) => {
+  Product.getProductById(req.params._id, (err, product) => {
+    if (err) {
+      throw err;
+    }
+    res.json(product);
+  });
+});
+
+router.post("/", (req, res) => {
+  let product = req.body;
+  Product.addProduct(product, (err, product) => {
+    if (err) {
+      throw err;
+    }
+    res.json(product);
+  });
+});
+
+router.put("/:_id", (req, res) => {
+  const id = req.params._id;
+  const product = req.body;
+  Product.updateProduct(id, product, {}, (err, product) => {
+    if (err) {
+      throw err;
+    }
+    res.json(product);
+  });
+});
+
+router.delete("/:_id", (req, res) => {
+  const id = req.params._id;
+  Product.removeProduct(id, (err, product) => {
+    if (err) {
+      throw err;
+    }
+    res.json(product);
+  });
+});
+
+module.exports = router;
