33629b423841da7d74711520074e9df371000dc1
Gabriel Fuertes <gabifuertes@gmail.com>
2018-08-07 18:55:43 -0300

    Fixed Product require.

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const Product = require("./models/product");
+const Product = require("../models/product");
 
 router.get("/", (req, res) => {
   Product.getProducts((err, products) => {
